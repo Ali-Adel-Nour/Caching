c985709fc8f19b1a0b634119b7b14450cc971821
Ali-Adel-Nour <aliiiadel841@gmail.com>
2023-05-11 17:35:13 +0300

    finshing last touchs

--- app.js
+++ app.js
@@ -8,26 +8,65 @@ const responseTime = require('response-time');
 
 const {promisify}= require('util');
 
+const Port = process.env.PORT || 3000
+
 const app = express();
 
 app.use(responseTime())
 
 const client = redis.createClient({
-  host:'127.0.0.1',
-  port:6379
+  host: '127.0.0.1',
+  port: 6379,
 })
+const GET_ASYNC = promisify(client.get).bind(client)
+const SET_ASYNC = promisify(client.set).bind(client)
 
-app.get('/rockets',async(req,res,next)=>{
-  try{
-    const response = await axios.get('https://api.spacexdata.com/v3')
+app.get('/rockets', async (req, res, next) => {
+  try {
+    const reply = await GET_ASYNC('rockets')
+    if (reply) {
+      console.log('using cached data')
+      res.send(JSON.parse(reply))
+      return
+    }
+    const response = await axios.get('https://api.spacexdata.com/v3/rockets')
+    const saveResult = await SET_ASYNC(
+      'rockets',
+      JSON.stringify(response.data),
+      'EX',
+      5
+    )
+    console.log('new data cached', saveResult)
     res.send(response.data)
-
-  }catch(error){
-  res.send(error.message)
+  } catch (error) {
+    res.send(error.message)
   }
 })
 
-const Port = process.env.PORT || 3000
+app.get('/rockets/:rocket_id', async (req, res, next) => {
+  try {
+    const { rocket_id } = req.params
+    const reply = await GET_ASYNC(rocket_id)
+    if (reply) {
+      console.log('using cached data')
+      res.send(JSON.parse(reply))
+      return
+    }
+    const response = await axios.get(
+      `https://api.spacexdata.com/v3/rockets/${rocket_id}`
+    )
+    const saveResult = await SET_ASYNC(
+      rocket_id,
+      JSON.stringify(response.data),
+      'EX',
+      5
+    )
+    console.log('new data cached', saveResult)
+    res.send(response.data)
+  } catch (error) {
+    res.send(error.message)
+  }
+})
 
 app.listen(Port,()=>{
   console.log(`listening on ${Port}`);
